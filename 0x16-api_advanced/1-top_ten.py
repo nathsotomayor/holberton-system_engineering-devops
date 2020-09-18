ed68d2dea4ae724b9cac1c07a2450c4995587f8b
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-09-17 23:53:05 -0500

    Prints the titles of the first 10 hot posts listed for a given subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Prints the titles of the first 10 hot posts listed
+for a given subreddit."""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that queries Reddit API"""
+    uri = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    header = {"User-Agent": "My-User-Agent"}
+
+    sub_info = requests.get(uri, headers=header, allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print("None")
+    else:
+        for element in sub_info.json().get('data').get('children'):
+            print(element.get("data").get("title"))
