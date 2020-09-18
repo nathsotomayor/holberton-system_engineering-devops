5d01c8ae10b803ab7e0b65fbceb1cac5c62ed8d0
Nathaly Sotomayor Ampudia <nathysoto12.07@gmail.com>
2020-09-17 23:39:16 -0500

    Queries the Reddit API Returns the number of subscribers (not active users, total subscribers) for a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that queries Reddit API"""
+    uri = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {"User-Agent": "My-User-Agent"}
+
+    sub_info = requests.get(uri, headers=header, allow_redirects=False)
+    if sub_info.status_code >= 300:
+        return 0
+
+    return sub_info.json().get('data', {}).get('subscribers', 0)
